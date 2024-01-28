12f9c778ca30c91cc77997135f91db34e1e756c9
wedsan <andre.alves.silvazesez@gmail.com>
2024-01-28 13:11:34 -0300

    initial commit

---
+++ src/main/java/wedsan/simplemarketplace/SimpleMarketplaceApplication.java
@@ -0,0 +1,33 @@
+package wedsan.simplemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMarketplaceApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
