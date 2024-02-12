693b9d819cf09c4350b6af00300e476ffbfad9fc
AndrelsonGIT <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-12 00:23:26 -0300

    feat: Create address class

---
+++ src/main/java/wedsan/simplemarketplace/core/domain/Address.java
@@ -0,0 +1,14 @@
+package wedsan.simplemarketplace.core.domain;
+
+public class Address {
+
+    private String zipCode;
+
+    private String streetAddress;
+
+    private String city;
+
+    private String Province;
+
+    private String addressNumber;
+}
