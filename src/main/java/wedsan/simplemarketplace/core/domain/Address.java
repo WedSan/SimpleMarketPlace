ee7a8811a5a0f05f3ad2d21ced91513bc24f4b6f
AndrelsonGIT <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-12 00:50:21 -0300

    feat: Add constructor and getter and setter to Address

--- src/main/java/wedsan/simplemarketplace/core/domain/Address.java
+++ src/main/java/wedsan/simplemarketplace/core/domain/Address.java
@@ -2,6 +2,16 @@ package wedsan.simplemarketplace.core.domain;
 
 public class Address {
 
+    public Address(){}
+
+    public Address(String zipCode, String streetAddress, String city, String province, String addressNumber) {
+        this.zipCode = zipCode;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        Province = province;
+        this.addressNumber = addressNumber;
+    }
+
     private String zipCode;
 
     private String streetAddress;
@@ -11,4 +21,44 @@ public class Address {
     private String Province;
 
     private String addressNumber;
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return Province;
+    }
+
+    public void setProvince(String province) {
+        Province = province;
+    }
+
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
 }
