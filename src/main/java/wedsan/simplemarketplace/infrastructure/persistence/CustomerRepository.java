f996d8368a9139861bc4ce1445547a0295651ad9
AndrelsonGIT <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-13 18:56:23 -0300

    fix: Add @Repository to CustomerRepository

--- src/main/java/wedsan/simplemarketplace/infrastructure/persistence/CustomerRepository.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/persistence/CustomerRepository.java
@@ -1,7 +1,9 @@
 package wedsan.simplemarketplace.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import wedsan.simplemarketplace.core.domain.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 }
