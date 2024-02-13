8db89727b6d2642b1629830954ed3dd7ffe89e1e
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-13 19:34:02 -0300

    teste

--- src/main/java/wedsan/simplemarketplace/core/domain/Seller.java
+++ src/main/java/wedsan/simplemarketplace/core/domain/Seller.java
@@ -8,7 +8,7 @@ public class Seller extends User{
 
     private List<Product> productsListSeller;
 
-    private BigDecimal accountBalance;
+    private BigDecimal accountBalanceqwrwqrwqe;
 
     public Seller() {
         super();
