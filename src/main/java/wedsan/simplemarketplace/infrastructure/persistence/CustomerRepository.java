236747e065c8ef062f29f3c63b92925a2092d0ed
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-17 19:57:02 -0300

    fix: Changed CustomerRepository interface to work with CustomerEntity instead of Customer domain class

--- src/main/java/wedsan/simplemarketplace/infrastructure/persistence/CustomerRepository.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/persistence/CustomerRepository.java
@@ -3,7 +3,8 @@ package wedsan.simplemarketplace.infrastructure.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wedsan.simplemarketplace.core.domain.Customer;
+import wedsan.simplemarketplace.infrastructure.entity.CustomerEntity;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Long>{
 }
