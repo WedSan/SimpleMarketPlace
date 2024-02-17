cecaf3d6937f89ad8f9f782891a23c89daaa5df5
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-17 20:04:30 -0300

    feat: Add CustomerEntityMapper and UserMapper beans

--- src/main/java/wedsan/simplemarketplace/infrastructure/config/UserConfig.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/config/UserConfig.java
@@ -5,14 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import wedsan.simplemarketplace.application.usecase.CreateCustomerUseCase;
 import wedsan.simplemarketplace.application.usecase.gateway.CustomerGateway;
 import wedsan.simplemarketplace.application.usecase.impl.CreateCustomerUseCaseImpl;
+import wedsan.simplemarketplace.infrastructure.entity.CustomerEntity;
+import wedsan.simplemarketplace.infrastructure.gateway.CustomerEntityMapper;
 import wedsan.simplemarketplace.infrastructure.gateway.CustomerRepositoryGateway;
+import wedsan.simplemarketplace.infrastructure.mapper.UserMapper;
 import wedsan.simplemarketplace.infrastructure.persistence.CustomerRepository;
 
 @Configuration
 public class UserConfig {
+
     @Bean
-    CustomerGateway customerGateway(CustomerRepository customerRepository){
-        return new CustomerRepositoryGateway(customerRepository);
+    CustomerEntityMapper customerEntityMapper(){
+        return new CustomerEntityMapper();
+    }
+
+    @Bean
+    UserMapper userMapper(){
+        return new UserMapper();
+    }
+
+    @Bean
+    CustomerGateway customerGateway(CustomerRepository customerRepository, CustomerEntityMapper customerEntityMapper){
+        return new CustomerRepositoryGateway(customerRepository, customerEntityMapper);
     }
 
     @Bean
