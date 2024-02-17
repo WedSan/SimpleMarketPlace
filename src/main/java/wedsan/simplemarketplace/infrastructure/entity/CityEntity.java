c9f727d354b9dfefaabc8f5d1c2dd2c3d08d6ef3
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-17 20:07:52 -0300

    feat: Create CityEntity to model city entities in the database.

---
+++ src/main/java/wedsan/simplemarketplace/infrastructure/entity/CityEntity.java
@@ -0,0 +1,41 @@
+package wedsan.simplemarketplace.infrastructure.entity;
+
+import jakarta.persistence.*;
+import wedsan.simplemarketplace.core.domain.Address;
+
+@Entity
+@Table(name = "tb_city")
+public class CityEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String cityName;
+
+    public CityEntity(){}
+
+    public CityEntity(String cityName){
+        this.cityName = cityName;
+    }
+    public CityEntity(Integer id, String cityName) {
+        this.id = id;
+        this.cityName = cityName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
