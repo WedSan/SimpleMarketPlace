3c5e2c65d13b8f93d6f80ccfd93e8dc875c2cd14
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-17 20:09:50 -0300

    feat: Add creation user exception handling in CreationUserExceptionHandler class

---
+++ src/main/java/wedsan/simplemarketplace/infrastructure/exceptionHandler/CreationUserExceptionHandler.java
@@ -0,0 +1,16 @@
+package wedsan.simplemarketplace.infrastructure.exceptionHandler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import wedsan.simplemarketplace.core.exception.UserDocumentInvalidException;
+
+@ControllerAdvice
+public class CreationUserExceptionHandler {
+
+    @ExceptionHandler(UserDocumentInvalidException.class)
+    public ResponseEntity<Object> handleUserCreationException(Exception ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+}
