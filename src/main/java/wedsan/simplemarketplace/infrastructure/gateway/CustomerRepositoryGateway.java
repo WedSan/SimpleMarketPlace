43cfd010e954ff975da40214250dde1dc9baeed2
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-17 20:02:44 -0300

    refactor: CustomerRepositoryGateway to utilize CustomerEntityMapper
    - Updated CustomerRepositoryGateway constructor to accept CustomerEntityMapper
    - Modified save method in CustomerRepositoryGateway to properly map Customer to CustomerEntity and vice versa using CustomerEntityMapper

--- src/main/java/wedsan/simplemarketplace/infrastructure/gateway/CustomerRepositoryGateway.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/gateway/CustomerRepositoryGateway.java
@@ -2,18 +2,25 @@ package wedsan.simplemarketplace.infrastructure.gateway;
 
 import wedsan.simplemarketplace.application.usecase.gateway.CustomerGateway;
 import wedsan.simplemarketplace.core.domain.Customer;
+import wedsan.simplemarketplace.infrastructure.entity.CustomerEntity;
 import wedsan.simplemarketplace.infrastructure.persistence.CustomerRepository;
 
 public class CustomerRepositoryGateway implements CustomerGateway {
 
     private final CustomerRepository customerRepository;
 
-    public CustomerRepositoryGateway(CustomerRepository customerRepository) {
+    private final CustomerEntityMapper customerEntityMapper;
+
+    public CustomerRepositoryGateway(CustomerRepository customerRepository, CustomerEntityMapper customerEntityMapper) {
         this.customerRepository = customerRepository;
+        this.customerEntityMapper = customerEntityMapper;
     }
 
     @Override
     public Customer save(Customer customerToBeSaved) {
-        return this.customerRepository.save(customerToBeSaved);
+        CustomerEntity customerEntityToBeSaved = this.customerEntityMapper.toEntity(customerToBeSaved);
+        CustomerEntity customerEntitySaved =  this.customerRepository.save(customerEntityToBeSaved);
+        Customer customerSaved = this.customerEntityMapper.toDomainObj(customerEntitySaved);
+        return customerSaved;
     }
 }
