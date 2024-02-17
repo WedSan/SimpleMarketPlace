c7c79cededa5f0e0996cc61df09d2b4c2107f1f6
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-17 19:55:28 -0300

    feat: Add @Entity and constructor to CustomerEntity

--- src/main/java/wedsan/simplemarketplace/infrastructure/entity/CustomerEntity.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/entity/CustomerEntity.java
@@ -1,14 +1,17 @@
 package wedsan.simplemarketplace.infrastructure.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import wedsan.simplemarketplace.core.domain.Product;
 
 import java.util.List;
-
+@Entity
 @Table(name = "tb_customer")
 public class CustomerEntity extends UserEntity{
 
+    public CustomerEntity() {
+    }
+
+    public CustomerEntity(Long id, String name, String document, String email, AddressEntity address) {
+        super(id, name, document, email, address);
+    }
 }
