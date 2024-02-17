c49e9a1809f9b45b061c38c57c5c6893fb7b447f
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-17 20:08:46 -0300

    feat: Create ProvinceEntity to model provinces entities in the database.

---
+++ src/main/java/wedsan/simplemarketplace/infrastructure/entity/ProvinceEntity.java
@@ -0,0 +1,38 @@
+package wedsan.simplemarketplace.infrastructure.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_province")
+public class ProvinceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String provinceName;
+
+
+    public ProvinceEntity() {
+    }
+
+    public ProvinceEntity(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
