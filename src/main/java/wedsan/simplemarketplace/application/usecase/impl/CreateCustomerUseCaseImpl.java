c242df5577d12097d8caf42b95a9903721ed8540
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-18 19:00:20 -0300

    feat: Add list of validators to validate customer creation

--- src/main/java/wedsan/simplemarketplace/application/usecase/impl/CreateCustomerUseCaseImpl.java
+++ src/main/java/wedsan/simplemarketplace/application/usecase/impl/CreateCustomerUseCaseImpl.java
@@ -3,17 +3,23 @@ package wedsan.simplemarketplace.application.usecase.impl;
 import wedsan.simplemarketplace.application.usecase.CreateCustomerUseCase;
 import wedsan.simplemarketplace.application.usecase.gateway.CustomerGateway;
 import wedsan.simplemarketplace.core.domain.Customer;
+import wedsan.simplemarketplace.core.domain.UserValidator;
+
+import java.util.List;
 
 public class CreateCustomerUseCaseImpl implements CreateCustomerUseCase {
 
     private final CustomerGateway customerGateway;
+    private final List<UserValidator> userValidatorList;
 
-    public CreateCustomerUseCaseImpl(CustomerGateway customerGateway) {
+    public CreateCustomerUseCaseImpl(CustomerGateway customerGateway, List<UserValidator> userValidatorList) {
         this.customerGateway = customerGateway;
+        this.userValidatorList = userValidatorList;
     }
 
     @Override
     public Customer create(Customer customerToBeSaved) {
+        userValidatorList.forEach(validator-> validator.validate(customerToBeSaved));
         return customerGateway.save(customerToBeSaved);
     }
 }
