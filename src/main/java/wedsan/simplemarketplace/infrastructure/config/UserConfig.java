ea963b5bd426a0078102098cc99c69a71e433839
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-18 13:25:01 -0300

    feat: Add list of UserValidator beans to UserConfig
    This commit adds a list of UserValidator beans to the UserConfig class. These validators are intended to be used for validating user input in various scenarios.
    
    Additionally, this commit updates the CreateCustomerUseCaseImpl class to inject the UserValidator beans as dependencies. This enables the use of these validators within the CreateCustomerUseCaseImpl for validating customer data before creation.

--- src/main/java/wedsan/simplemarketplace/infrastructure/config/UserConfig.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/config/UserConfig.java
@@ -4,34 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import wedsan.simplemarketplace.application.usecase.CreateCustomerUseCase;
 import wedsan.simplemarketplace.application.usecase.gateway.CustomerGateway;
+import wedsan.simplemarketplace.application.usecase.gateway.ShopkeeperGateway;
+import wedsan.simplemarketplace.application.usecase.gateway.UserGateway;
 import wedsan.simplemarketplace.application.usecase.impl.CreateCustomerUseCaseImpl;
-import wedsan.simplemarketplace.infrastructure.entity.CustomerEntity;
+import wedsan.simplemarketplace.core.domain.DuplicateEmailValidator;
+import wedsan.simplemarketplace.core.domain.UserValidator;
 import wedsan.simplemarketplace.infrastructure.gateway.CustomerEntityMapper;
 import wedsan.simplemarketplace.infrastructure.gateway.CustomerRepositoryGateway;
+import wedsan.simplemarketplace.infrastructure.gateway.ShopkeeperRepositoryGateway;
 import wedsan.simplemarketplace.infrastructure.mapper.UserMapper;
 import wedsan.simplemarketplace.infrastructure.persistence.CustomerRepository;
+
+import java.util.List;
 
 @Configuration
 public class UserConfig {
 
     @Bean
-    CustomerEntityMapper customerEntityMapper(){
+    CustomerEntityMapper customerEntityMapper() {
         return new CustomerEntityMapper();
     }
 
     @Bean
-    UserMapper userMapper(){
-        return new UserMapper();
+    List<UserValidator> userValidator(UserGateway userGateway){
+       return List.of(new DuplicateEmailValidator(userGateway));
     }
 
     @Bean
-    CustomerGateway customerGateway(CustomerRepository customerRepository, CustomerEntityMapper customerEntityMapper){
-        return new CustomerRepositoryGateway(customerRepository, customerEntityMapper);
-    }
-
-    @Bean
-    CreateCustomerUseCase createCustomerUseCase(CustomerGateway customerGateway){
-        return new CreateCustomerUseCaseImpl(customerGateway);
+    CreateCustomerUseCase createCustomerUseCase(CustomerGateway customerGateway, List<UserValidator> userValidatorList){
+        return new CreateCustomerUseCaseImpl(customerGateway, userValidatorList);
     }
 
 }
