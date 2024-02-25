cb4893cc5ac61f83930b05c6b96e898c76b75a4e
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-25 16:50:56 -0300

    feat: Add a CreateShopkeeperUseCase bean to UserConfig for creating an instance of CreateShopkeeperUseCase

--- src/main/java/wedsan/simplemarketplace/infrastructure/config/UserConfig.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/config/UserConfig.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class UserConfig {
 
     @Bean
-    CreateShopkeeperUseCase createCustomerUseCase(ShopkeeperGateway shopkeeperGateway, List<UserValidator> listUserValidator){
+    CreateShopkeeperUseCase createShopkeeperUseCase(ShopkeeperGateway shopkeeperGateway, List<UserValidator> listUserValidator){
         return new CreateShopkeeperUseCaseImpl(shopkeeperGateway,listUserValidator);
     }
 
