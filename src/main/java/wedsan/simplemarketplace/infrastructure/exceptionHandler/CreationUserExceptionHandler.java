895d0f814dbaec4cb9c1b65c06edc5acdd832c2c
WedSan <148652838+AndrelsonGIT@users.noreply.github.com>
2024-02-25 16:48:55 -0300

    feat: Add DuplicateDocumentException to be handled by the handleUserCreationException method

--- src/main/java/wedsan/simplemarketplace/infrastructure/exceptionHandler/CreationUserExceptionHandler.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/exceptionHandler/CreationUserExceptionHandler.java
@@ -3,12 +3,13 @@ package wedsan.simplemarketplace.infrastructure.exceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import wedsan.simplemarketplace.core.DuplicateDocumentException;
 import wedsan.simplemarketplace.core.exception.UserDocumentInvalidException;
 
 @ControllerAdvice
 public class CreationUserExceptionHandler {
 
-    @ExceptionHandler(UserDocumentInvalidException.class)
+    @ExceptionHandler({UserDocumentInvalidException.class, DuplicateDocumentException.class})
     public ResponseEntity<Object> handleUserCreationException(Exception ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
