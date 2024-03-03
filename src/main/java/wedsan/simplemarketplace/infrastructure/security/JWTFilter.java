53fc9ca5b5573cd10474d3a5dcb4fa0eba60ed0f
WedSan <120421201+WedSan@users.noreply.github.com>
2024-03-03 20:13:08 -0300

    Feat: Add a filter to verify authentication token from request

---
+++ src/main/java/wedsan/simplemarketplace/infrastructure/security/JWTFilter.java
@@ -0,0 +1,61 @@
+package wedsan.simplemarketplace.infrastructure.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+
+    private final UserDetailsService userDetailsService;
+
+    public JWTFilter(JWTService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractTokenFromRequestHeader(request);
+
+        if(StringUtils.hasText(token) && jwtService.isTokenValid(token)){
+            String email = jwtService.getSubject(token);
+            UserDetails user = userDetailsService.loadUserByUsername(token);
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(
+                    user.getUsername(),
+                    null,
+                    user.getAuthorities()
+            );
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromRequestHeader(HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        String token = null;
+
+        if(StringUtils.hasText(header) && header.startsWith("Bearer ")){
+            token = header.substring(7);
+        }
+
+        return token;
+    }
+}
