ccb54bc793caeee4e851368af51154d7f2917d3c
WedSan <120421201+WedSan@users.noreply.github.com>
2024-03-12 08:46:56 -0300

    Feat: Add Authentication Manager bean to authenticate users

--- src/main/java/wedsan/simplemarketplace/infrastructure/config/security/SecurityConfiguration.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/config/security/SecurityConfiguration.java
@@ -2,14 +2,16 @@ package wedsan.simplemarketplace.infrastructure.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import wedsan.simplemarketplace.infrastructure.security.JWTKey;
-import wedsan.simplemarketplace.infrastructure.security.PasswordHasher;
 
 @Configuration
 @EnableWebSecurity
@@ -20,8 +22,13 @@ public class SecurityConfiguration {
         httpSecurity.csrf(csrf->csrf.disable());
         httpSecurity.sessionManagement(sm-> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authorizeHttpRequests(auth-> auth.requestMatchers("api/user/**").permitAll());
-
+        httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers("api/auth/**").permitAll());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 
     @Bean
