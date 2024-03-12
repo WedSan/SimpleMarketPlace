7b3ac84bc0d8cc94e6861671c1e380f0dbacac54
WedSan <120421201+WedSan@users.noreply.github.com>
2024-03-12 09:07:23 -0300

    feat: Add class to represent an authentication object com token and refresh token

---
+++ src/main/java/wedsan/simplemarketplace/core/domain/AuthenticationToken.java
@@ -0,0 +1,29 @@
+package wedsan.simplemarketplace.core.domain;
+
+public class AuthenticationToken {
+
+    private String token;
+
+    private String refreshToken;
+
+    public AuthenticationToken(String token, String refreshToken) {
+        this.token = token;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
