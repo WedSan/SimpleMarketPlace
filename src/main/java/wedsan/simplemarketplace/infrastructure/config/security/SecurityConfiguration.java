1373f3752051b9f224dfd172ad8e894524193da3
WedSan <120421201+WedSan@users.noreply.github.com>
2024-03-12 10:19:09 -0300

    feat: Add JWTFilter to Filter Chain Spring Security, to apply JWTFilter to requests

--- src/main/java/wedsan/simplemarketplace/infrastructure/config/security/SecurityConfiguration.java
+++ src/main/java/wedsan/simplemarketplace/infrastructure/config/security/SecurityConfiguration.java
@@ -9,18 +9,29 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import wedsan.simplemarketplace.infrastructure.security.JWTFilter;
+import wedsan.simplemarketplace.infrastructure.security.JWTService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    private JWTFilter jwtFilter;
+
+    public SecurityConfiguration(JWTFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf->csrf.disable());
         httpSecurity.sessionManagement(sm-> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         httpSecurity.authorizeHttpRequests(auth-> auth.requestMatchers("api/user/**").permitAll());
         httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers("api/auth/**").permitAll());
         return httpSecurity.build();
@@ -35,4 +46,5 @@ public class SecurityConfiguration {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 }
